ceef0531aa63ef8498d257659490385284ac36b6
axlecho <axlecho@gmail.com>
2014-10-07 13:03:38 +0800

    修改按钮的样式

--- src/me/yiye/LoginManagerActivity.java
+++ src/me/yiye/LoginManagerActivity.java
@@ -3,14 +3,6 @@ package me.yiye;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.ImageView;
-import android.widget.TextView;
-
-import com.actionbarsherlock.app.SherlockActivity;
-import com.actionbarsherlock.app.ActionBar.LayoutParams;
-import com.actionbarsherlock.view.MenuItem;
 
 public class LoginManagerActivity extends BaseActivity{
 
